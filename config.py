77822e158f1caa76c5cf424b2789958192b6acae
kppeterkiss <axx6v4@inf.elte.hu>
2024-11-19 08:14:51 +0100

    db init, API queries started

---
+++ config.py
@@ -0,0 +1,6 @@
+gtfs_download_link='https://{username}:{password}@www.mavcsoport.hu/gtfs/gtfsMavMenetrend.zip'
+data_root="data/"
+weather_folder=data_root+"odp/"
+gtfs_folder=data_root+"gtfsMavMenetrend/"
+gtfs_stops_file="stops.txt"
+gtfs_dowload_location=data_root+"gtfs/"
